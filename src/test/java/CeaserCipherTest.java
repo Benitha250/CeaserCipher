e0bb9e36ea8ff4951149c7b8d141f61d3cc63759
Benitha250 <benithauwase05@gmail.com>
2020-11-16 11:40:45 +0200

    add codes to test if you shift 1 character

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,5 +1,12 @@
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class CeaserCipherTest {
 
+    @Test
+    public void rotateCharacter_CorrectNumberofTimesAtEndOfAlphabet_char(){
+        char input='a';
+        char expectedOutput='b';
+        assertEquals(expectedOutput, CeaserCipher.cipher(input, 1));
+    }
 }
