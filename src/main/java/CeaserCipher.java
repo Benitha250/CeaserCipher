21755261621664e7a97255427f6041d57dcf11d7
Benitha250 <benithauwase05@gmail.com>
2020-11-16 11:34:47 +0200

    add codes to ceaser class

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -19,4 +19,21 @@ public class CeaserCipher {
     public int getShift() {
         return shift;
     }
+
+
+    public static char cipher(char c, int k) {
+        //Here we declare some helping constants
+        final int alphaLength = 26;
+        final char asciiShift = Character.isUpperCase(c) ? 'A' : 'a';
+        final int cipherShift = k % alphaLength;
+
+        // we shift down to 0..25 for a..z
+        char shifted = (char) (c - asciiShift);
+
+        // Here we rotate the letter and handle "wrap-around" for negatives and value >= 26
+        shifted = (char) ((shifted + cipherShift + alphaLength) % alphaLength);
+
+        // Here we shift back up to english characters
+        return (char) (shifted + asciiShift);
+    }
 }
