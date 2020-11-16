0dae5c7f3048c62001f0b398dbedcae52c39b6e3
Benitha250 <benithauwase05@gmail.com>
2020-11-16 11:26:11 +0200

    add codes to get the text

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,3 +1,14 @@
+//package CeaserCipher;
+
+import java.util.Scanner;
+
 public class CeaserCipher {
 
+    private String text;
+    private int shift;
+
+    public CeaserCipher(String text, int shift) {
+        this.text = text;
+        this.shift = shift;
+    }
 }
